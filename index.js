8f525d3bc705c6a1c61335a1062c6dee64f95b9d
Linh <cao00087@algonquinlive.com>
2023-02-02 16:15:01 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+ function playSound(e) {
+    console.log(e);
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`.key[data-key=${e.code}]`);
+
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.toggle("playing");
+}
+
+function removeTransition(e) {
+    if(e.propertyName != "transform") return;
+    this.classList.remove('playing');
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+
+window.addEventListener('keydown', playSound);
